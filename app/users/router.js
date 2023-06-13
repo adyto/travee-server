44002c0fca67040b2e67d54bde417818400a4d5a
ady.to <adiyulianto61@gmail.com>
2023-06-13 11:25:12 +0700

    add router in users api

--- app/users/router.js
+++ app/users/router.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { bestWisata, detailProduct } = require('./controller');
+const { bestWisata, detailProduct, bestAttraction } = require('./controller');
 
 const { isLoginUser } = require('../middleware/auth');
 
 router.get('/best-wisata', bestWisata);
+router.get('/best-atraksi', bestAttraction);
 router.get('/product/:id', detailProduct);
 
 module.exports = router;
