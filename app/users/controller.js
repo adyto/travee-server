758fed8871b9417a8ce5a048a60aeef8a2219f65
ady.to <adiyulianto61@gmail.com>
2023-06-20 12:06:16 +0700

    push

--- app/users/controller.js
+++ app/users/controller.js
@@ -1,6 +1,10 @@
 const Product = require('../product/model');
 const Category = require('../category/model');
+const User = require('./model');
 const Ticket = require('../ticket/model');
+const config = require('../../config');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
   bestWisata: async (req, res) => {
@@ -51,6 +55,60 @@ module.exports = {
       });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal Server Error` });
+    }
+  },
+  avatarUser: async (req, res, next) => {
+    const { id } = req.params;
+    console.log(id);
+    try {
+      if (req.file) {
+        let tmp_path = req.file.path;
+        let originalExt =
+          req.file.originalname.split('.')[
+            req.file.originalname.split('.').length - 1
+          ];
+        let filename = req.file.filename + '.' + originalExt;
+        console.log(filename);
+        let target_path = path.resolve(
+          config.rootPath,
+          `public/uploads/${filename}`,
+        );
+
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
+
+        // src.pipe(dest);
+        const prevAvatar = await User.findOne({ _id: id });
+        console.log(prevAvatar.avatar !== undefined);
+        if (prevAvatar.avatar !== undefined) {
+          // masukin upload baru
+        }
+        src.on('end', async () => {
+          try {
+            // const userID = { _id: id };
+            // const resAvatar = { avatar: filename };
+            // await User.findOneAndUpdate(userID, resAvatar, {
+            //   new: true,
+            //   upsert: true,
+            //   rawResult: true, // Return the raw result from the MongoDB driver
+            // });
+          } catch (err) {
+            if (err && err.name === 'ValidationError') {
+              return res.status(422).json({
+                error: 1,
+                message: err.message,
+                fields: err.errors,
+              });
+            }
+            next(err);
+          }
+        });
+      }
+    } catch (err) {
+      res.status(505).json({
+        message: err.message || `Internal server error`,
+      });
+      next();
     }
   },
 };
--- app/users/router.js
+++ app/users/router.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
-const { bestWisata, detailProduct, bestAttraction } = require('./controller');
+const {
+  bestWisata,
+  detailProduct,
+  bestAttraction,
+  avatarUser,
+} = require('./controller');
+const multer = require('multer');
+const os = require('os');
 
 const { isLoginUser } = require('../middleware/auth');
 
 router.get('/best-wisata', bestWisata);
 router.get('/best-atraksi', bestAttraction);
 router.get('/product/:id', detailProduct);
+router.post(
+  '/:id/avatar',
+  multer({ dest: os.tmpdir() }).single('image'),
+  avatarUser,
+);
 
 module.exports = router;
